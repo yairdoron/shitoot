52e7b4aad83982a2f23aa418be07805267cacc28
nimi <nimshen@gmail.com>
2017-07-28 17:30:25 +0300

    amending textbox h

--- Common/TextBox.h
+++ Common/TextBox.h
@@ -3,13 +3,13 @@
 
 #include <string>
 #include <Windows.h>
-#include "Control.h"
+#include "Label.h"
 
 using namespace std;
 
 
 
-class TextBox : public Control
+class TextBox : public Label
 {
 public:
 	//constractor
@@ -17,13 +17,11 @@ public:
 
 	virtual ~TextBox() = default;
 
-
-	// Functions
-	void SetText(string value) { _text = value; };
-	string GetText() { return _text;  };
-
+    // Callbacks
+	virtual void draw(Graphics &g, int left, int top, size_t layer) const;
+	virtual void mousePressed(int x, int y, bool isLeft);
+	virtual void keyDown(int keyCode, char character);
 	
 private:
 	int _cursorPosition;
-	string _text;
 };

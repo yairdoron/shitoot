a7e576ed4b44eb33da1d462afed62a87a4d7b108
nimi <nimshen@gmail.com>
2017-07-28 17:42:13 +0300

    textbox constructor

--- Common/TextBox.cpp
+++ Common/TextBox.cpp
@@ -1,7 +1,11 @@
 #include "TextBox.h"
 
 
-TextBox::TextBox( int width) : Control(1 , width)
+TextBox::TextBox(int width) : Label(width), _cursorPosition(0)
 {
-
+	setCanGetFocus(true);
+}
+void TextBox::draw(Graphics & g, int left, int top, size_t layer) const
+{
+//to do
 }
